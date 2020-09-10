77ae09ba7f6d1362be2b40f0735cdcdb03e66900
Shubham Mishra <sm091274@gmail.com>
2020-09-11 02:13:00 +0530

    Add solution for problem `A - String Task` Link: https://codeforces.com/contest/118/problem/A Tags: implementation, strings, *1000 Ref: https://codeforces.com/contest/118/submission/92458639

---
+++ codeforces/118/A.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Code15
+ */
+public class Code15 {
+    public static Boolean isVovel(char ar) {
+        return ar=='a' || ar=='e' || ar=='i' || ar=='o' || ar=='u' || ar == 'y' ;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char [] ar= sc.next().toLowerCase().toCharArray();
+
+        for (char c : ar) {
+            
+            if(!Code15.isVovel(c))
+            System.out.print('.'+ String.valueOf(c));
+        }
+        
+    }
+}
