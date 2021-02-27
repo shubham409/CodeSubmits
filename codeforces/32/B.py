21572617b041974e4364bef3c5951e1698139852
Shubham Mishra <sm091274@gmail.com>
2021-02-27 23:54:00 +0530

    Add solution for problem `B - Borze` Link: https://codeforces.com/contest/32/problem/B Tags: expression parsing, implementation, *800 Ref: https://codeforces.com/contest/32/submission/108624339

---
+++ codeforces/32/B.py
@@ -0,0 +1,17 @@
+def fun(t:str):
+    st = t.replace('--','2')
+    st = st.replace('-.','1')
+    st = st.replace('.','0')
+    print(st)
+
+
+# for i in range(T):
+#     # t=input()
+#     # r= input()
+#     # r= list(map(int,list(r)))
+#     # b= input()
+#     # b= list(map(int,list(b)))    
+#     # st= input()
+t= input()
+# fun(st)
+fun(t)
