b81a7743e022c07d134625ecc16481c12a0c987f
Shubham Mishra <sm091274@gmail.com>
2021-02-27 23:32:00 +0530

    Add solution for problem `A - A+B (Trial Problem)` Link: https://codeforces.com/contest/1351/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1351/submission/108623341

---
+++ codeforces/1351/A.py
@@ -0,0 +1,14 @@
+def fun(ls):
+    print(sum(ls))
+
+
+    
+
+    
+T= int(input())
+for i in range(T):
+    # t= int(input())
+    # n= int(input())
+    # m= int(input())
+    ls= list(map(int,input().split()))
+    fun(ls)
