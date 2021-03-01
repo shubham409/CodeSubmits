3480519fa87e071a777f95fa152cbb63c538aaac
Shubham Mishra <sm091274@gmail.com>
2021-03-01 15:12:00 +0530

    Add solution for problem `B - String LCM` Link: https://codeforces.com/contest/1473/problem/B Tags: brute force, math, number theory, strings, *1000 Ref: https://codeforces.com/contest/1473/submission/108790074

--- codeforces/1473/B.py
+++ codeforces/1473/B.py
@@ -1,9 +1,8 @@
-from math import gcd
+from math import floor,gcd
+
+from collections import Counter
+
 def fun(s,t):
-    # concept->
-    # 1. if one is multiple of another then answer is bigger string
-    # 2. if not multiple than lcm(len(s),len(t)) may be string divisible by both
-    # 3. else no divisibility
     if(len(s)>=len(t)):
         s,t=t,s
         sl=len(s)
@@ -13,15 +12,23 @@ def fun(s,t):
         sl=len(s)
         tl=len(t)
     q=sl//tl    
+
     lcm = sl*tl//gcd(sl,tl)
     qs=lcm//sl
     qt=lcm//tl
     if (q*t==s):
         print(s)
+
     elif (s*qs==t*qt):
         print(s*qs)
     else:
         print(-1)
+    
+
+
+    
+
+
 T= int(input())
 for i in range(T):
     # t= int(input())
