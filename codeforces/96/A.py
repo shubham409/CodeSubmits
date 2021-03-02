b5dcddc9e1012d16eb07546eca5349e8bbf499c3
Shubham Mishra <sm091274@gmail.com>
2021-03-02 23:09:00 +0530

    Add solution for problem `A - Football` Link: https://codeforces.com/contest/96/problem/A Tags: implementation, strings, *900 Ref: https://codeforces.com/contest/96/submission/108950977

---
+++ codeforces/96/A.py
@@ -0,0 +1,12 @@
+def fun(st):
+    if('1111111'in st or '0000000'in st):
+        print('YES')
+    else:
+        print('NO')
+        
+
+# T= int(input())
+# for i in range(T):
+    # ls=list(map(int,input().split()))
+st=input()
+fun((st))
