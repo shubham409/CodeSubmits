78fbfed720061ce21f265bc7ec1ee36dabc5da74
Shubham Mishra <sm091274@gmail.com>
2021-03-03 23:53:00 +0530

    Add solution for problem `A - Kids Seating` Link: https://codeforces.com/contest/1443/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1443/submission/109033152

--- codeforces/1443/A.py
+++ codeforces/1443/A.py
@@ -1,8 +1,3 @@
-def fun(n):
-   ls=[i for i in range(2*n,4*n,2)]
-   print(*ls)
-        
-T = int(input())
-for i in range(T):
-    t= int(input())
-    fun(t)
+for i in range(int(input())):
+    n = int(input())
+    print(*range(2*n, 4*n, 2))
