79e3903f24c10ca903449619d9b1f66397612fd8
Shubham Mishra <sm091274@gmail.com>
2021-03-06 23:21:00 +0530

    Add solution for problem `A - Boy or Girl` Link: https://codeforces.com/contest/236/problem/A Tags: brute force, implementation, strings, *800 Ref: https://codeforces.com/contest/236/submission/109284487

---
+++ codeforces/236/A.py
@@ -0,0 +1,13 @@
+def fun(n):
+    if(len(set(n))%2==0):
+        print('CHAT WITH HER!')
+    else:
+        print('IGNORE HIM!')
+
+# T = int(input())
+# for i in range(T):
+# n = list(map(int, input().split()))
+# n = int(input())
+# ls = list(map(int, input().split()))
+n=input()
+fun(n)
