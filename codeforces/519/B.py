a76f029f4f8e0d324e9eb8c7518b7d8b94164b8c
Shubham Mishra <sm091274@gmail.com>
2021-04-25 23:19:00 +0530

    Add solution for problem `B - A and B and Compilation Errors` Link: https://codeforces.com/contest/519/problem/B Tags: data structures, implementation, sortings, *1100 Ref: https://codeforces.com/contest/519/submission/114224884

--- codeforces/519/B.py
+++ codeforces/519/B.py
@@ -1,6 +1,15 @@
+from collections import Counter
+from math import ceil
+from decimal import Decimal
+
 def fun(a,b,c):
-    print(sum(a)-sum(b))
-    print(sum(b)-sum(c))
+    dct1=Counter(a)
+    dct2=Counter(b)
+    dct3=Counter(c)
+    k=list(dct1.items()-dct2.items())
+    print((k[0][0]))
+    k=list(dct2.items()-dct3.items())
+    print((k[0][0]))
 
 
         
