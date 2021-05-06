30895a4ae0b20374ba1ade1080bb5246e33305fb
Shubham Mishra <sm091274@gmail.com>
2021-05-06 15:53:00 +0530

    Add solution for problem `E - Arranging The Sheep` Link: https://codeforces.com/contest/1520/problem/E Tags: greedy, math, *1400 Ref: https://codeforces.com/contest/1520/submission/115402625

---
+++ codeforces/1520/E.py
@@ -0,0 +1,48 @@
+def fun(st,n):
+    suffix=[0 for i in range(n)]
+    prefix=[0 for i in range(n)]
+    count=0
+    for i,val in enumerate(st):
+        if(val=='*'):
+            if(i==0):
+                prefix[i]=0
+                count+=1
+            else:
+                prefix[i]=prefix[i-1]
+                count+=1
+        else:
+            if(i==0):
+                prefix[i]=0
+                count+=0
+            else:
+                prefix[i]=prefix[i-1]+count
+                count+=0
+    count=0
+    for i in range(n-1,-1,-1):
+        val=st[i]
+        if(val=='*'):
+            if(i==n-1):
+                suffix[i]=0
+                count+=1
+            else:
+                suffix[i]=suffix[i+1]
+                count+=1
+        else:
+            if(i==n-1):
+                suffix[i]=0
+                count+=0
+            else:
+                suffix[i]=suffix[i+1]+count
+                count+=0
+    ans=10**12
+    for i in range(n):
+        if(i!=n-1):
+            ans=min(ans,prefix[i]+suffix[i+1])
+        else:
+            ans=min(ans,prefix[i])
+    print(ans)
+T = int(input())
+for _ in range(T):
+    n=int(input())
+    st=input()
+    fun(st,n)
