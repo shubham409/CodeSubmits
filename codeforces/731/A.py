0daa92d045979cfe4e7c162eabcc1f2fad014e0c
Shubham Mishra <sm091274@gmail.com>
2021-05-10 23:56:00 +0530

    Add solution for problem `A - Night at the Museum` Link: https://codeforces.com/contest/731/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/731/submission/115890741

--- codeforces/731/A.py
+++ codeforces/731/A.py
@@ -1,17 +1,7 @@
-import string
-def fun(name):
-    ls=list(string.ascii_lowercase[:])
-    dct={val:i for i ,val in enumerate(ls)}
-    count=0
-    prv=0
-    for i in name:
-        val=dct.get(i)
-        now=min(abs(val-prv),abs(26-abs(prv-val)))
-        count+=now
-        prv=val
-    print(count)
-
-T=1
-for i in range(T):
-    name=input()
-    fun(name)
+c=97
+s=0
+for x in map(ord,input()):
+    i=abs(c-x)
+    s+=min(i,26-i)
+    c=x
+print(s)    
