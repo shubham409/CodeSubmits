36f689274edbd2f818241e3c14ee9f3d912370ca
Shubham Mishra <sm091274@gmail.com>
2021-05-12 00:07:00 +0530

    Add solution for problem `A - Keyboard` Link: https://codeforces.com/contest/474/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/474/submission/115971475

--- codeforces/474/A.py
+++ codeforces/474/A.py
@@ -5,8 +5,22 @@ def fun(st,n):
         ans+=s[s.find(i)+[1,-1][n=='R']]
     print(ans)
 
+
+
+
+
+
+
+    
+
+# T = int(input())
 T=1
 for i in range(T):
     n=input()
     st=input()
+    # val=int(input())
+    # st=input()
+    # ts=input()
+    # ks= list(map(int, input().split()))    
+    # ls= list(map(int, input().split()))    
     fun(st,n)
