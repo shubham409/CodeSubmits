1b7cc36fa7024fd2c8f6137806239908fa22e068
Shubham Mishra <sm091274@gmail.com>
2021-05-15 14:46:00 +0530

    Add solution for problem `A - Three Piles of Candies` Link: https://codeforces.com/contest/1196/problem/A Tags: brute force, constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1196/submission/116246864

---
+++ codeforces/1196/A.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <map>
+#include <array>
+#include <list>
+#include <set>
+#include <deque>
+#include <iterator>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <ostream>
+#include <iomanip>
+#include <cstdlib>
+#include <sstream>
+#include <cmath>
+#include <cstring>
+#include <cstdio>
+#include <cassert>
+#include <valarray>
+using namespace std;
+
+#define vectorofint vector<int>
+#define vectorofstring vector<string>
+#define vectoroflong vector<long>
+#define vectoroflonglong vector<long long>
+#define input(x) cin >> x
+#define print(x) cout << x << " "
+#define println(x) cout << x << endl
+#define newline() cout << endl;
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define loopfrom(i, j, n) for (int i = j; i < n; i++)
+#define get(i) \
+    int i;     \
+    input(i)
+#define gets(i, j) \
+    int i, j;      \
+    input(i);      \
+    input(j)
+#define getall(i, j, k) \
+    int i, j, k;        \
+    input(i);           \
+    input(j);           \
+    input(k)
+template <typename first, typename second, typename input>
+map<first, second> counter(input &obj)
+{
+    map<first, second> ans;
+    for (first i : obj)
+    {
+        ans[i]++;
+    }
+    return ans;
+}
+template<typename type>
+void getvector(vector<type> &v, type n)
+{
+    loop(i, n)
+    {
+        type next;
+        input(next);
+        v[i] = next;
+    }
+}
+void inputvector(vector<int> &v, int n)
+{
+    loop(i, n)
+    {
+        int next;
+        input(next);
+        v[i] = next;
+    }
+}
+void prefixsum(vector<int> &v, int n)
+{
+    loopfrom(i, 1, n)
+    {
+        v[i] = v[i - 1] + v[i];
+    }
+}
+int stringtoint(string &s)
+{
+    stringstream mystream(s);
+    int ans;
+    mystream >> ans;
+    return ans;
+}
+void solve(vectorofint &v, int target)
+{
+}
+void input_predicate(int &i)
+{
+    input(i);
+}
+void output_predicate(int &i)
+{
+    println(i);
+}
+void increment_predicate(int &i)
+{
+    i = i + 1;
+}
+void solve()
+{
+    vector<long long > v(3);
+    getvector<long long>(v,3);
+    long long x=0;
+    x=accumulate(v.begin(),v.end(),x);
+    println(x/2);
+
+}
+int main()
+{
+    get(t);
+    loop(i, t)
+    {
+        solve();
+    }
+}
